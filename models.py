2bdea8c3af730c2fba0d72a66ba129301e2131b7
Fabio Rueda <avances123@gmail.com>
2011-11-27 01:53:01 +0100

    acercamiento a los modelos

--- models.py
+++ models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Sistema(models.Model):
+	nombre = models.CharField(max_length=200)
+	#pub_date = models.DateTimeField('date published')
+
+
+class Sitio(models.Model):
+	sistema = models.ForeignKey(Sistema)
+	nombre = models.CharField(max_length=200)
+	votos = models.IntegerField()
+
+class Partido(models.Model):
+	sitio = models.ForeignKey(Sitio)
+	nombre = models.CharField(max_length=200)
+
