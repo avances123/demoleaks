c3b2a6dd9d9b89122bc6add3f551afb5499b34cc
Fabio Rueda <avances123@gmail.com>
2011-11-27 02:11:03 +0100

    Acabados los modelos

---
+++ admin.py
@@ -0,0 +1,6 @@
+from formulas_electorales.models import Sistema,Sitio,Partido
+from django.contrib import admin
+
+admin.site.register(Sistema)
+admin.site.register(Sitio)
+admin.site.register(Partido)
--- models.py
+++ models.py
@@ -4,15 +4,24 @@ from django.db import models
 
 class Sistema(models.Model):
 	nombre = models.CharField(max_length=200)
-	#pub_date = models.DateTimeField('date published')
+	pub_date = models.DateTimeField('Fecha elecciones')
 
 
 class Sitio(models.Model):
 	sistema = models.ForeignKey(Sistema)
-	nombre = models.CharField(max_length=200)
-	votos = models.IntegerField()
+	nombre_sitio = models.CharField(max_length=200)
+	num_a_elegir = models.IntegerField()
+	tipo_sitio = models.IntegerField()
+	votos_contabilizados = models.IntegerField()
+	votos_abstenciones = models.IntegerField()
+	votos_nulos = models.IntegerField()
+	votos_blancos = models.IntegerField()
+	
 
 class Partido(models.Model):
+	id_partido = models.IntegerField()
 	sitio = models.ForeignKey(Sitio)
 	nombre = models.CharField(max_length=200)
-
+	electos = models.IntegerField()
+	votos_numero = models.IntegerField()
+	votos_porciento = models.DecimalField(max_digits=5,decimal_places=2)
