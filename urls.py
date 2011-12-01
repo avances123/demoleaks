f0a7f04b9227e045f42b8705449a6e470bb1ce60
Fabio Rueda <avances123@gmail.com>
2011-12-01 20:38:55 +0100

    Empezando el frontend

--- urls.py
+++ urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('',
-		    url(r'^elecciones/$', 'formulas_electorales.views.index'),
-		    url(r'^elecciones/(?P<sistema_id>\d+)/$', 'formulas_electorales.views.detail'),
+		    url(r'^$', 'formulas_electorales.views.index'),
+		    url(r'^(?P<sistema_id>\d+)/$', 'formulas_electorales.views.detail'),
 		  )
--- views.py
+++ views.py
@@ -1,1 +1,17 @@
-# Create your views here.
+from django.http import HttpResponse
+from formulas_electorales.models import Sitio,Sistema,Partido
+from django.template import Context, loader
+from django.shortcuts import render_to_response
+
+
+def index(request):
+    	lista_sistemas = Sistema.objects.all()
+	c = Context({'lista_sistemas': lista_sistemas,})
+	return render_to_response('index.html',c)
+
+
+    
+
+def detail(request, sistema_id):
+	return HttpResponse("You're looking at poll %s." % sistema_id)
+
