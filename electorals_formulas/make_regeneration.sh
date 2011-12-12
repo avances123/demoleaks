57ce705324ba3d0c8eb34fc172cfe1689114848f
David Barragán Merino <bameda@dbarragan.com>
2011-12-12 02:33:12 +0100

    Fixed regenerate script

--- electorals_formulas/make_regeneration.sh
+++ electorals_formulas/make_regeneration.sh
@@ -4,11 +4,13 @@
 echo  "drop database electorals_formulas; create database electorals_formulas;" | psql -U postgres
 
 # CREATE DATABASE
-echo no | python manage.py syncdb
+echo no | python manage.py syncdb --migrate
 
-# POPULATE DATABASE FROM ELPAIS XMLS
+# POPULATE DATABASE FROM ELPAIS XMLS OR FROM FIXCTURE
 if [ "$1" == "--populate" ]; then
     python manage.py populate_from_elpais_xml 2008 2011
+else
+    python manage.py loaddata electoral/fixtures/fixture_electoral.json 
 fi
 
 # REGENERATE ALL.POs
