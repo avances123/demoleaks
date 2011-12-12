c643b52f0d7e6007f8e94e6fc97880e658ba5591
Fabio Rueda <avances123@gmail.com>
2011-12-13 00:01:17 +0100

    inserted electoral.views in patterns

--- electoral/urls.py
+++ electoral/urls.py
@@ -5,7 +5,7 @@ from django.views.generic import TemplateView
 from django.conf import settings
 from electoral.views.views import *
 
-urlpatterns = patterns('cuvitt.views',
+urlpatterns = patterns('electoral.views',
     # Home
     url(r'^$', IndexView.as_view(), name='index'),
     url(r'^(?P<sitio_id>\d+)/$', SitioView.as_view(), name='sitio'),
