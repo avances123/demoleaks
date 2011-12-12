074128ce98e932bbe2bef427916fba8a7e3b2490
Fabio Rueda <avances123@gmail.com>
2011-12-13 00:22:14 +0100

    ComicioView added

--- electoral/views/views.py
+++ electoral/views/views.py
@@ -9,6 +9,25 @@ from django.shortcuts import render_to_response, get_object_or_404
 
 from electoral.models import *
 
+class ComicioView(TemplateView):
+    template_name = 'electoral/comicio.html'
+
+    def get(self, request, comicio_id, *args, **kwargs):
+        context = self.get_context_data(sitio_id)
+        return self.render_to_response(context)
+
+    def post(self, request, *args, **kwargs):
+        context = self.get_context_data()
+        return self.render_to_response(context)
+
+    def get_context_data(self,sitio_id, **kwargs):
+        comicio = get_object_or_404(Comicio,id=comicio_id)
+        context = super(ComicioView,self).get_context_data(**kwargs)
+        context.update({'comicio':comicio})
+        return context
+
+
+
 
 class SitioView(TemplateView):
     template_name = 'electoral/sitio.html'
