d0f5aae527fc19860c757b722d28ba6b9fc8a664
Fabio Rueda <avances123@gmail.com>
2011-12-13 20:07:35 +0100

    Changed fixtures format to xml

--- make_fixture.sh
+++ make_fixture.sh
@@ -3,7 +3,7 @@
 if [ "$1" == "--load" ]; then
     python manage.py loaddata electoral/fixtures/fixture_electoral.json
 else
-    python manage.py dumpdata --indent=4 countries > countries/fixtures/initial_data.json
-    python manage.py dumpdata --indent=4 auth sites > electoral/fixtures/initial_data.json
-    python manage.py dumpdata --indent=4 electoral > electoral/fixtures/fixture_electoral.json
+    python manage.py dumpdata --format=xml --indent=4 countries > countries/fixtures/initial_data.json
+    python manage.py dumpdata --format=xml --indent=4 auth sites > electoral/fixtures/initial_data.json
+    python manage.py dumpdata --format=xml --indent=4 electoral > electoral/fixtures/fixture_electoral.json
 fi
