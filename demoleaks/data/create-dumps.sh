1701ecffb301c996f249616f5a457739c8f9332a
Fabio Rueda <avances123@gmail.com>
2012-05-17 21:17:10 +0200

    create sql dumps per model

---
+++ demoleaks/data/create-dumps.sh
@@ -0,0 +1,5 @@
+pg_dump -t data_party -f Party.sql  --inserts demoleaks
+pg_dump -t data_place -f Place.sql  --inserts demoleaks
+pg_dump -t data_election -f Election.sql  --inserts demoleaks
+pg_dump -t data_result -f Result.sql  --inserts demoleaks
+pg_dump -t data_resultparties -f ResultParties.sql  --inserts demoleaks
